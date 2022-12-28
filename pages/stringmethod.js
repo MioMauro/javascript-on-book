d30b49b40c9fe3dc9c072f2745125b096027fce1
Mauro.V <remanomauro@gmail.com>
2022-12-28 09:32:29 +0100

    block of code

---
+++ pages/stringmethod.js
@@ -0,0 +1,35 @@
+//let userName = "Bro Code"
+//console.log(userName.length)
+// result: 8
+
+//let userName = "Bro Code"
+//console.log(userName.chart(0)) // index = 0
+// result: B
+
+//let userName = "Bro Code"
+//console.log(userName.indexOf("o"))
+// result: 2
+
+//let userName = "Bro Code"
+//console.log(userName.lastIndexOf("d"))
+// result: 6
+
+//let userName = "Bro Code"
+//userName = userName.toUpperCase();
+//console.log(userName)
+// result: BRO CODE
+
+//let userName = "Bro Code"
+//userName = userName.toLowerCase()
+//console.log userName.trim()
+// result: 5
+
+//let phoneNumber = "123-456-7890"
+//phoneNumber = phoneNumber.replaceAll("-", "/") // replace from - TO /
+//console.log(phoneNumber)
+// result: 123/456/7890
+
+//let phoneNumber = "123-456-7890"
+//phoneNumber = phoneNumber.replaceAll("-", "") // replace from - TO nothing
+//console.log(phoneNumber)
+// result: 1234567890
