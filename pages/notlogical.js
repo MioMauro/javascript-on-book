6c566bd5f68fd5ea42bc81be70715807f41c67d0
Mauro.V <remanomauro@gmail.com>
2022-12-31 10:40:59 +0100

    code  block

---
+++ pages/notlogical.js
@@ -0,0 +1,38 @@
+/*
+let temp = 15
+
+if(temp > 0){
+    console.log("It's warm outside")
+}
+else {
+    console.log("It's cold outside")
+}
+
+
+let temp = -15
+
+if(!(temp > 0)){
+    console.log("It's cold outside")
+}
+else {
+    console.log("It's warm outside")
+}
+*/
+
+
+let temp = 15
+let sunny = false
+
+if(!(temp > 0)){
+    console.log("It's cold outside")
+}
+else {
+    console.log("It's warm outside")
+}
+
+if(!sunny){
+    console.log("it's cloudy outside")
+}
+else {
+    console.log("it's sunny outside")
+}
---
+++ pages/whileloop.js
@@ -0,0 +1,28 @@
+// while loop
+
+/*
+let userName = ""; // Will be insert
+
+while(userName == "" ){ // attend to insert name to continue
+    userName = window.prompt("Enter your name")
+}
+
+console.log("Hello", userName)
+*/
+
+/*
+let userName = "";
+
+while(userName == "" || userName == null ){ 
+    userName = window.prompt("Enter your name")
+}
+
+console.log("Hello", userName)
+
+*/
+
+/*
+while(1 == 1){
+    console.log("Help! I'm stuck in an infinite loop!")
+}
+*/
