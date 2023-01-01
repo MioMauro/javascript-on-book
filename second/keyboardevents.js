182c8a2d16f15f2dfba0400a4df74e76cbd6c93f
Mauro.V <remanomauro@gmail.com>
2023-01-01 17:33:56 +0100

    code block

--- second/keyboardevents.js
+++ second/keyboardevents.js
@@ -14,72 +14,63 @@ window.addEventListener('keydown', function(e) {
 })
 */
 
-let position = 1
-let length = 0
+let position = 1;
+        let length = 0;
 
-
-window.addEventListener('keydown', function(e) {
-    switch(e.code) {
-        case 'ArrowLeft':
-            if (length > 0) {
-                moveLeft()
+        window.addEventListener('keydown', function(e) {
+            switch (e.code) {
+                case 'ArrowLeft':
+                    if (length > 0) {
+                        moveLeft();
+                    }
+                    break;
+                case 'ArrowRight':
+                    if (length > 0) {
+                        moveRight();
+                    }
+                    break;
+                case 'Space':
+                    addBox();
+                    break;
+                case 'Backspace':
+                    if (length > 0) {
+                        removeBox();
+                    }
             }
-            break;
+        });
 
-            case 'ArrowRight':
-            if (length > 0) {
-                moveRight()
+        function addBox() {
+            const newDiv = document.createElement('div');
+            const section = document.getElementById('section');
+            section.append(newDiv);
+            if (length === 0) {
+                newDiv.classList.add('active');
             }
-            break;
+            length++;
+        }
 
-            case 'Backspace':
-            if (length > 0) {
-                removeBox()
+        function removeBox() {
+            if (position === length) {
+                moveLeft();
             }
-            break;
-    }
-}) 
+            document.querySelector('div:last-child').remove();
+            length--;
+        }
 
-// add functions
+        function moveLeft() {
+            document.querySelector(`div:nth-of-type(${position})`).classList.remove('active');
+            position--;
+            if (position < 1) {
+                position = length;
+            }
+            document.querySelector(`div:nth-of-type(${position})`).classList.add('active');
+        }
 
-function addBox() {
-    const newDiv = document.createElement('div')
-
-    const section = document.getElementById('section')
-    section.append(newDiv)
-
-    if (length === 0) {
-        newDiv.classList.add('active')
-    }
-    length++
-}
-
-function removeBox() {
-    if (position === length) {
-        moveLeft()
-    }
-    document.querySelector('div:last-child').remove()
-    length --
-}
-
-function moveLeft() {
-    document.querySelector(`div:nth-of-type(${position})`).classList.remove('active')
-    position --
-
-    if (position < 1) {
-        position = length
-    }
-
-    document.querySelector(`div:nth-of-type(${position})`).classList.add('active')
-}
-
-function moveRight() {
-    document.querySelector(`div:nth-of-type(${position})`).classList.remove('active')
-    position ++
-
-    if (position > length) {
-        position = 1
-    }
-
-    document.querySelector(`div:nth-of-type(${position})`).classList.add('active')
-}
+        function moveRight() {
+            document.querySelector(`div:nth-of-type(${position})`).classList.remove('active');
+            position++;
+            if (position > length) {
+                position = 1;
+            }
+            document.querySelector(`div:nth-of-type(${position})`).classList.add('active');
+        }
